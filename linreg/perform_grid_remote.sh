dd7edbedce912dc9f877970064cf349e36a33b6e
Mrinank Sharma <mrinank.sharma.97+dev@gmail.com>
2019-03-06 16:47:34 +0000

    update output dir

--- linreg/perform_grid_remote.sh
+++ linreg/perform_grid_remote.sh
@@ -5,5 +5,5 @@ export PYTHONPATH='/homes/ms2314/dp-pvi-project'
 echo $PYTHONPATH
 
 # try different means
-python /homes/ms2314/dp-pvi-project/linreg/grid_search_ana.py
-python /homes/ms2314/dp-pvi-project/linreg/grid_search_global_ana.py
+python /homes/ms2314/dp-pvi-project/linreg/grid_search_ana.py /scratch/ms2314/
+python /homes/ms2314/dp-pvi-project/linreg/grid_search_global_ana.py --output-base-dir /scratch/ms2314/
