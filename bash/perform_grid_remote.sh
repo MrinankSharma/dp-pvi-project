fb291e2fdc5e110022c45170f071adaacf2ef0b6
Mrinank Sharma <mrinank.sharma.97+dev@gmail.com>
2019-03-12 16:50:24 +0000

    add overwrite

--- bash/perform_grid_remote.sh
+++ bash/perform_grid_remote.sh
@@ -6,4 +6,4 @@ echo $PYTHONPATH
 
 # try different means
 python /homes/ms2314/dp-pvi-project/linreg/grid_search_ana.py --output-base-dir /scratch/ms2314/ --tag inf_private
-python /homes/ms2314/dp-pvi-project/linreg/grid_search_global_ana.py --output-base-dir /scratch/ms2314/ --tag inf_private
+python /homes/ms2314/dp-pvi-project/linreg/grid_search_global_ana.py --output-base-dir /scratch/ms2314/ --tag inf_private --overwrite
