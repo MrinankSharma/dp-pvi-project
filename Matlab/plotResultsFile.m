e350562cb41030a7c37d77650a79bce4b4da125a
Mrinank Sharma <mrinank.sharma.97+dev@gmail.com>
2019-03-12 12:21:26 +0000

    update pull remote file

--- Matlab/plotResultsFile.m
+++ Matlab/plotResultsFile.m
@@ -1,5 +1,5 @@
 % function plotResultsFile(filename)
-    filename = '/Users/msharma/workspace/IIB/dp-pvi-project/linreg/logs/gs_global_ana/default/results.csv'
+    filename = '/Users/msharma/workspace/IIB/dp-pvi-project/linreg/logs/gs_global_ana/default/results.csv';
     results_mat = csvread(filename);
 %     max eps: 1 eps: 2 eps_var: 3 dp_noise: 4 c: 5 kl: 6 kl_var: 7
 %     experiment_counter: 8 ignore L for the time being
@@ -9,8 +9,9 @@
     noise_vals = results_mat(:, 4);
     counter_vals = results_mat(:, 8);
     
-    figure('pos', [10 10 1200 800]);
-    scatter(eps_vals, kl_vals, 100*log10(noise_vals(L_vals==10)*10^5), log10(c_vals(L_vals==10)), 'o', 'filled');
+    f = figure('pos', [10 10 1200 800]);
+    scatter(eps_vals, kl_vals, 100*log10(noise_vals*10^5), log10(c_vals), 'o', 'filled');
+    legend()
     
     colormap parula
     c = colorbar;
@@ -19,5 +20,11 @@
     set(gca,'yscale','log')
     ylabel('$\mathcal{KL}(p(\theta| \mathcal{D})||q(\theta))$')
     xlabel('$\epsilon, \delta=10^{-5}$')
-    title('Local DP: Color is Clipping, Size is Noise, Circle (L=10), Diamond (L=100), Star (L=500) ')
+    title('Local DP: Color is Clipping, Size is Noise')
+    
+    datacursormode on
+    dcm = datacursormode(f);
+    mydatatip = @(a, b) experimentcounterdt(a, b, eps_vals, kl_vals, counter_vals);
+    set(dcm,'UpdateFcn', mydatatip)
+    
 % end
--- bash/pull_remote.sh
+++ bash/pull_remote.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-scp -rp ms2314@hinton:/scratch/ms2314/gs_local_ana /Users/msharma/workspace/IIB/dp-pvi-project/remote-results/gs_local_ana
-scp -rp ms2314@hinton:/scratch/ms2314/gs_global_ana /Users/msharma/workspace/IIB/dp-pvi-project/remote-results/gs_global_ana
+scp -rp ms2314@hinton:/scratch/ms2314/logs/gs_local_ana /Users/msharma/workspace/IIB/dp-pvi-project/remote-results/gs_local_ana
+scp -rp ms2314@hinton:/scratch/ms2314/logs/gs_global_ana /Users/msharma/workspace/IIB/dp-pvi-project/remote-results/gs_global_ana
