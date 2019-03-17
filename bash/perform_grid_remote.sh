56485fdc2e42568cd07eb73643655a5b2b655cd5
Mrinank Sharma <mrinank.sharma.97+dev@gmail.com>
2019-03-17 23:57:47 +0000

    update remote bash script

--- bash/perform_grid_remote.sh
+++ bash/perform_grid_remote.sh
@@ -5,5 +5,6 @@ export PYTHONPATH='/homes/ms2314/dp-pvi-project'
 echo $PYTHONPATH
 
 # try different means
-python /homes/ms2314/dp-pvi-project/linreg/grid_search_ana.py --output-base-dir /scratch/ms2314/ --tag less_data
-python /homes/ms2314/dp-pvi-project/linreg/grid_search_global_ana.py --output-base-dir /scratch/ms2314/ --tag less_data
+# less data - more epsilons
+python /homes/ms2314/dp-pvi-project/linreg/grid_search_ana.py --output-base-dir /scratch/ms2314/ --tag less_data_more_eps
+python /homes/ms2314/dp-pvi-project/linreg/grid_search_global_ana.py --output-base-dir /scratch/ms2314/ --tag less_data_more_eps
