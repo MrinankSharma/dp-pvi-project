477850de09c3c54c66c70a171469c1405e0d2a4f
Mrinank Sharma <mrinank.sharma.97+dev@gmail.com>
2019-03-25 22:28:55 +0000

    bash bug

--- bash/perform_grid_remote.sh
+++ bash/perform_grid_remote.sh
@@ -6,6 +6,6 @@ echo $PYTHONPATH
 
 # try different means
 # less data - more epsilons
-python /homes/ms2314/dp-pvi-project/linreg/grid_search_ana.py --output-base-dir /scratch/ms2314/ --tag damped -no-workers 40
+python /homes/ms2314/dp-pvi-project/linreg/grid_search_ana.py --output-base-dir /scratch/ms2314/ --tag damped --no-workers 40
 python /homes/ms2314/dp-pvi-project/linreg/grid_search_ana.py --output-base-dir /scratch/ms2314/ --tag damped_more_workers
 python /homes/ms2314/dp-pvi-project/linreg/grid_search_global_ana.py --output-base-dir /scratch/ms2314/ --tag more_workers_damped_two
