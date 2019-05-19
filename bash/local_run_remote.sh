b70b4e65759f0333038887c12290d15257948cb9
Mrinank Sharma <mrinank.sharma.97+dev@gmail.com>
2019-05-19 10:47:22 +0100

    update bash

--- bash/local_run_remote.sh
+++ bash/local_run_remote.sh
@@ -4,4 +4,4 @@
 export PYTHONPATH='/homes/ms2314/dp-pvi-project'
 echo $PYTHONPATH
 
-python /homes/ms2314/dp-pvi-project/linreg/local_sample_invest.py --output-base-dir /scratch/ms2314/ --overwrite --tag local-bias-sampled --N-dp-seeds 40
+python /homes/ms2314/dp-pvi-project/linreg/local_sample_invest.py --output-base-dir /scratch/ms2314/ --overwrite --tag local-bias-sampled --N-seeds 40
