77ab8e01249b7a8f3d7bd526afa503fccd2671fe
Mrinank Sharma <mrinank.sharma.97+dev@gmail.com>
2019-05-21 13:54:00 +0100

    update settings

--- bash/local_run_remote.sh
+++ bash/local_run_remote.sh
@@ -5,4 +5,4 @@ export PYTHONPATH='/homes/ms2314/dp-pvi-project'
 echo $PYTHONPATH
 
 python /homes/ms2314/dp-pvi-project/linreg/local_sample_invest.py --output-base-dir /scratch/ms2314/ --overwrite --tag final-noisy --N-seeds 50
-python /homes/ms2314/dp-pvi-project/linreg/local_sample_invest.py --output-base-dir /scratch/ms2314/ --no-noise --overwrite --tag final-not-noisy --N-seeds 50
+python /homes/ms2314/dp-pvi-project/linreg/local_sample_invest.py --output-base-dir /scratch/ms2314/ --no-noise --overwrite --tag ficd nal-not-noisy --N-seeds 50
